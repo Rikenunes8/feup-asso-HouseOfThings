316716b73fb920aa68cc7827b703ae237e99b727
Henrique Nunes <hrn2001@gmail.com>
2023-02-20 20:08:12 +0000

    Add device server socket

---
+++ fake_devices/fakeLampSocket.py
@@ -0,0 +1,27 @@
+import socket
+
+
+def server_program():
+  host = socket.gethostname()
+  port = 5001  
+
+  server_socket = socket.socket() 
+  server_socket.bind((host, port))
+
+  server_socket.listen(2)
+  conn, address = server_socket.accept()  
+  print("Connection from: " + str(address))
+  while True:
+    data = conn.recv(1024).decode()
+    if not data:
+      # if data is not received break
+      break
+    print("from connected user: " + str(data))
+    data = "ACK" if "connect" else "REJ"
+    conn.send(data.encode())  
+
+  conn.close()
+
+
+if __name__ == '__main__':
+  server_program()

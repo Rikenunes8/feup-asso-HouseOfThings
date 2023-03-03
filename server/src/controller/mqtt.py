2a1c299633067aa34d6fc7373ed334d393e4eb61
Henrique Nunes <hrn2001@gmail.com>
2023-03-03 12:08:29 +0000

    Fix output typo

--- server/src/controller/mqtt.py
+++ server/src/controller/mqtt.py
@@ -38,7 +38,7 @@ def publish(client, topic, msg):
     # result: [0, 1]
     status = result[0]
     if status == 0:
-      print(f"Send `{msg}` to topic `{topic}`")
+      print(f"Successfully sent `{msg}` to topic `{topic}`")
       break
     else:
       print(f"Failed to send message to topic {topic}")

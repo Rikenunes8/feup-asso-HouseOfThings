d7c60a661c3ddb601fc929dfb9842752710c9b4c
Henrique Nunes <hrn2001@gmail.com>
2023-03-09 16:49:18 +0000

    refactor: extract flask app to outside of src

--- backend/src/api.py
+++ backend/src/api.py
@@ -1,40 +1,39 @@
-from flask import Flask, jsonify, make_response, request
-from HoT import HoT 
+from flask import Blueprint, jsonify, make_response, request
+from src.HoT import HoT 
 
-app = Flask(__name__)
-hot = HoT()
+api = Blueprint('api', __name__)
 
 def notJson():
   return make_response(jsonify({'error': 'Content-Type not supported!'}), 400)
 def isContentJson(request):
   return request.headers.get('Content-Type') == 'application/json'
 
-@app.get("/heartbeat")
+@api.get("/heartbeat")
 def heartbeat() -> str:
   return jsonify({'status': 'ok'})
 
 
-@app.post("/devices/<id>/connect")
+@api.post("/devices/<id>/connect")
 def connect(id):
   if (not isContentJson(request)): return notJson()
-  hot.connect(id, request.json)
+  HoT().connect(id, request.json)
   return jsonify({})
 
-@app.post("/devices/<id>/disconnect")
+@api.post("/devices/<id>/disconnect")
 def disconnect(id):
-  hot.disconnect(id)
+  HoT().disconnect(id)
   return jsonify({})
 
-@app.post("/devices/<id>/action")
+@api.post("/devices/<id>/action")
 def action(id):
   if (not isContentJson(request)): return notJson()
-  hot.action(id, request.json)
+  HoT().action(id, request.json)
   return jsonify({})
   
 
-@app.get("/devices")
+@api.get("/devices")
 def connectedDevices() -> str:
-  devices = hot.devices()
+  devices = HoT().devices()
   return jsonify({'devices': list(map(lambda device: device.toJson(), devices))})
   
 

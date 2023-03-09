38887f951bce720a33f9625085d6f5c8b4f7639f
Henrique Nunes <hrn2001@gmail.com>
2023-03-09 16:48:18 +0000

    fix: actually use singleton

--- backend/src/HoT.py
+++ backend/src/HoT.py
@@ -1,15 +1,19 @@
-from controller.adapter.DeviceAdapter import DeviceAdapter
-from model.DeviceManager import DeviceManager
-from controller.adapter.LightMqttAdapter import LightMqttAdapter
-from database.DB import DB
+from src.controller.adapter.DeviceAdapter import DeviceAdapter
+from src.controller.adapter.LightMqttAdapter import LightMqttAdapter
+from src.model.DeviceManager import DeviceManager
+from src.database.DB import DB
 
-class HoT():
-  def __new__(cls):
-    if not hasattr(cls, 'instance'):
-      cls.instance = super(HoT, cls).__new__(cls)
-    return cls.instance
+class HoTMeta(type):
+  _instances = {}
+  def __call__(cls, *args, **kwargs):
+    if cls not in cls._instances:
+      instance = super().__call__(*args, **kwargs)
+      cls._instances[cls] = instance
+    return cls._instances[cls]
 
+class HoT(metaclass=HoTMeta):
   def __init__(self):
+    print("HoT init")
     self._devManager = DeviceManager()
     self._cid = "HoT" # TODO: set this to something better
     self._loadDevices()
--- backend/src/database/DB.py
+++ backend/src/database/DB.py
@@ -1,14 +1,17 @@
+import os
 import pymongo
 from dotenv import load_dotenv
-import os
 
-class DB():
+class DBMeta(type):
+  _instances = {}
+  def __call__(cls, *args, **kwargs):
+    if cls not in cls._instances:
+      instance = super().__call__(*args, **kwargs)
+      cls._instances[cls] = instance
+    return cls._instances[cls]
+
+class DB(metaclass=DBMeta):
   _name = 'HoT'
-
-  def __new__(cls):
-    if not hasattr(cls, 'instance'):
-      cls.instance = super(DB, cls).__new__(cls)
-    return cls.instance
 
   def __init__(self):
     load_dotenv('.env')

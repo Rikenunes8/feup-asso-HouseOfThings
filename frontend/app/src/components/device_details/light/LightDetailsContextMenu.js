30e78ac011d3e5e5c0be5d3ee4d03ea085bf47e4
Patricia Oliveira <up201905427@fe.up.pt>
2023-03-14 02:34:13 +0000

    feat: add loading spinner till disconnect response

--- frontend/app/src/components/device_details/light/LightDetailsContextMenu.js
+++ frontend/app/src/components/device_details/light/LightDetailsContextMenu.js
@@ -1,4 +1,6 @@
-import React, { useContext } from "react";
+import React, { useState, useContext } from "react";
+import {ActivityIndicator, StyleSheet, View} from 'react-native';
+
 import ContextMenu from "../../ContextMenu";
 import DevicesContext from "../../../contexts/DevicesContext";
 
@@ -14,48 +16,71 @@ export default function LightDetailsContextMenu({
 }) {
   const { removeDevice } = useContext(DevicesContext);
 
+  const [isLoading, setIsLoading] = useState(false);
+
   return (
-    <ContextMenu
-      isContextMenuVisible={isContextMenuVisible}
-      setIsContextMenuVisible={setIsContextMenuVisible}
-      options={[
-        {
-          name: "Rename",
-          icon: "edit-2",
-          color: colors.primaryText,
-          callback: () => console.log("TODO: Rename"),
-        },
-        {
-          name: "Disconnect",
-          icon: "wifi-off",
-          color: colors.red,
-          callback: () => {
-            utils.showConfirmDialog(
-              "Disconnect device", 
-              "Are you sure you want to disconnect this device?", 
-              () => { 
-                console.log("Disconnecting device..."); 
-                api.disconnectDevice(deviceContextMenuUid).then((success) => {
-                  if (success) {
-                    console.log("Device disconnected successfully");
-                    setIsContextMenuVisible(false); 
-                    setIsDetailsModalVisible(false); 
-                    removeDevice(deviceContextMenuUid);
-                    return;
-                  }
-                  
-                  console.log("Failed to disconnect device");
-                  setIsContextMenuVisible(false);
-                  utils.showErrorMessage("Failed to disconnect device");
-                });
-              }, 
-              () => { 
-                console.log("Canceling disconnect..."); 
-              }
-            );
+    <>
+      <ContextMenu
+        isContextMenuVisible={isContextMenuVisible}
+        setIsContextMenuVisible={setIsContextMenuVisible}
+        options={[
+          {
+            name: "Rename",
+            icon: "edit-2",
+            color: colors.primaryText,
+            callback: () => console.log("TODO: Rename"),
           },
-        },
-      ]}
-    />
+          {
+            name: "Disconnect",
+            icon: "wifi-off",
+            color: colors.red,
+            callback: () => {
+              utils.showConfirmDialog(
+                "Disconnect device", 
+                "Are you sure you want to disconnect this device?", 
+                () => { 
+                  console.log("Disconnecting device..."); 
+                  setIsLoading(true);
+
+                  api.disconnectDevice(deviceContextMenuUid).then((success) => {
+                    setIsLoading(false);
+                    setIsContextMenuVisible(false); 
+
+                    if (success) {
+                      console.log("Device disconnected successfully");
+                      setIsDetailsModalVisible(false); 
+                      removeDevice(deviceContextMenuUid);
+                      return;
+                    }
+                    
+                    console.log("Failed to disconnect device");
+                    utils.showErrorMessage("Failed to disconnect device");
+                  });
+                }, 
+                () => { 
+                  console.log("Canceling disconnect..."); 
+                }
+              );
+            },
+          },
+        ]}
+      />
+
+      {isLoading && 
+        <View style={styles.loadingContainer} >
+          <ActivityIndicator size="large" color={colors.white} />
+        </View>
+      }
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  // TODO: better position loading indicator so it 
+  // covers the entire screen!
+  loadingContainer: {
+    ...StyleSheet.absoluteFill,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});

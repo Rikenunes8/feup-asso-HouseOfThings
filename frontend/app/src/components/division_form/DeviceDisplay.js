404a93411724b5a124614ace35a362c6839e0a86
Victor Nunes <victorsnunes99@gmail.com>
2023-04-09 22:36:07 +0100

    Add DeviceDisplay

--- frontend/app/src/components/division_form/DeviceDisplay.js
+++ frontend/app/src/components/division_form/DeviceDisplay.js
@@ -3,13 +3,55 @@ import {
   StyleSheet,
   Text,
   View,
+  Image
 } from "react-native";
+import utils from "../../utils/utils";
 
 import colors from "../../../configs/colors";
 
-export default function DeviceDisplay({deviceName, deviceGroup}) {
+export default function DeviceDisplay({device}) {
 
   return (
-    <Text key={deviceName + deviceGroup}>{deviceName} e {deviceGroup}</Text>
+    <View style={styles.card} key={device.name + device.divisions[0]}>
+      <Image style={styles.icon} source={utils.getDeviceIcon(device.subcategory)} />
+      <View>
+        <Text style={styles.name}>{device.name}</Text>
+        <Text style={styles.divisions}>{device.divisions[0]}</Text>
+      </View>
+      {displayOnOff(device.on)}
+    </View>
   );
 }
+
+function displayOnOff(power) {
+  if (power)
+    return <Image source={require("../../../../assets/green-check.png")} />
+  else
+  return <Image source={require("../../../../assets/red-check.png")} />
+}
+
+const styles = StyleSheet.create({
+  card: {
+    borderColor: "lightgrey",
+    borderWidth: 1,
+    borderRadius: 5,
+    width: "50%",
+    paddingVertical: 5,
+    paddingHorizontal: 8,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  icon: {
+    width: 20,
+    height: 40,
+  },
+  name: {
+    fontWeight: 800,
+  },
+  divisions: {
+    color: "grey",
+    fontSize: 10
+  }
+});
--- frontend/app/src/components/division_form/DevicesDisplayInForm.js
+++ frontend/app/src/components/division_form/DevicesDisplayInForm.js
@@ -1,27 +1,50 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import {
   StyleSheet,
   Text,
   View,
+  Image,
 } from "react-native";
 
 import api from "../../api/api";
 import colors from "../../../configs/colors";
 import DeviceDisplay from "./DeviceDisplay";
+import DevicesContext from "../../contexts/DivisionsContext"
 
 export default function DevicesDisplayInForm() {
+/*
+  const { devices, setDevices } = useContext(DevicesContext);
+  const fetchDevices = async () => {
+    
+    const devs = await api.getDevices();
+    setDevices(devs);
+    
+    setDevices([{name: "Philips", group: "light"}])
+  };
 
-  devices=[{name: "Philips", group: "Family Room"}]
+  useEffect(() => {
+    fetchDevices();
+  }, []);
+*/
+  const showDevices = () => {
+    let devices = [{name: "Philips Bulb", divisions: ["Family Room"], subcategory: "light bulb", on: true}]
+    if (devices) {
+      return devices.map((device) => (
+        <DeviceDisplay device={device} />
+      ))
+    }
+  }
 
   return (
     <View
       style={styles.deviceDisplayContainer}
     >
-        <Text style={styles.title}>DEVICES</Text>
+        <View style={styles.search}>
+          <Text style={styles.title}>DEVICES</Text>
+          <Image source={require("../../../../assets/search.png")} />
+        </View>
 
-        {devices.map((device) => (
-          <DeviceDisplay deviceName={device.name} deviceGroup={device.group} />
-        ))}
+        {showDevices()}
     </View>
   );
 }
@@ -37,5 +60,10 @@ const styles = StyleSheet.create({
     backgroundColor: colors.white,
     margin: 10,
     padding: 10,
+  },
+  search: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between"
   }
 });

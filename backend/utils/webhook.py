170adb66a2b08c6f83764c3e48054869130cb57b
Henrique Nunes <hrn2001@gmail.com>
2023-05-18 14:19:42 +0100

    chore: remove unused file

--- backend/utils/webhook.py
+++
@@ -1,4 +0,0 @@
-from discord import SyncWebhook
-
-webhook = SyncWebhook.from_url("https://discord.com/api/webhooks/1108084159903178892/-wfJopfOfAmXNI-XYh2sZA20Q1CxMmgOYN9eEu0EoRJ69TatLzWaVoh89_mqunzP8RG6")
-webhook.send("Hello World")

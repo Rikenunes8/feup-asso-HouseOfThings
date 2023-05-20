1a8161f13741a0f7fdd0f8c76a086eb66b2dd300
Patricia Oliveira <up201905427@fe.up.pt>
2023-05-20 13:05:35 +0100

    feat: color and brightness change after time no changes logic

--- frontend/app/src/components/device_details/LightBulbRGBDetails.js
+++ frontend/app/src/components/device_details/LightBulbRGBDetails.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
 import { ColorPicker, toHsv, fromHsv } from "react-native-color-picker";
 import Slider from "react-native-a11y-slider";
@@ -25,21 +25,45 @@ export default function LightBulbRGBDetails({
     powerHandler(power, setDisabled);
   };
 
-  // Set color of the device (some time after the color picker has is last change)
-  // actionDevice(device.uid, { action: "set_color", "data": { "color": color (hex ?) } })
-  const onPickerColorChange = (color) => {
-    console.log(`Color changed: ${fromHsv(color)}`);
-    setPickerColor(color);
-    // apos x tempo sem change -> fazer o handle de change da cor ao server (pela api)
-  };
+  useEffect(() => {
+    let colorTimeoutId;
 
-  // Set brightness of the device (some time after the slider has is last change)
-  // actionDevice(device.uid, { action: "set_brightness", "data": { "brightness": int } })
-  const onSliderChange = (values) => {
-    console.log(`Brightness changed: ${values[0]}`);
-    setSliderValue(values[0]);
-    // apos x tempo sem change -> fazer o handle de change da brigtness ao server (pela api)
-  };
+    // Change color - after 500ms of no change
+    const colorHandler = () => {
+      clearTimeout(colorTimeoutId);
+      colorTimeoutId = setTimeout(() => {
+        if (color == fromHsv(pickerColor)) return;
+        console.log(`Color changed: ${fromHsv(pickerColor)}`);
+        // actionDevice(device.uid, { action: "set_color", "data": { "color": color (hex ?) } })
+      }, 500); // 500ms
+    };
+
+    colorHandler();
+
+    return () => {
+      clearTimeout(colorTimeoutId);
+    };
+  }, [pickerColor]);
+
+  useEffect(() => {
+    let brightnessTimeoutId;
+
+    // Change brightness - after 500ms of no change
+    const brightnessHandler = () => {
+      clearTimeout(brightnessTimeoutId);
+      brightnessTimeoutId = setTimeout(() => {
+        if (brightness == sliderValue) return;
+        console.log(`Brightness changed: ${sliderValue}`);
+        // actionDevice(device.uid, { action: "set_brightness", "data": { "brightness": brightness } })
+      }, 500); // 500ms
+    };
+
+    brightnessHandler();
+
+    return () => {
+      clearTimeout(brightnessTimeoutId);
+    };
+  }, [sliderValue]);
 
   const brightnessIconName = (brightness) => {
     if (brightness < 40) return "brightness-low";
@@ -60,7 +84,7 @@ export default function LightBulbRGBDetails({
         <ColorPicker
           color={pickerColor}
           defaultColor={toHsv(color)}
-          onColorChange={onPickerColorChange}
+          onColorChange={(color) => setPickerColor(color)}
           onColorSelected={onPickerColorSelected}
           hideSliders={true}
           style={{ flex: 1 }}
@@ -87,7 +111,7 @@ export default function LightBulbRGBDetails({
           markerColor={colors.primaryText}
           trackStyle={styles().trackBrightnessSlider}
           showLabel={false}
-          onChange={onSliderChange}
+          onChange={(values) => setSliderValue(values[0])}
         />
         <Text style={styles().lightText}>{sliderValue}</Text>
       </View>

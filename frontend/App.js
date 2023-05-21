8df9d1ae94ba39e4bf97ffe44a45a2064f506bd9
Henrique Nunes <hrn2001@gmail.com>
2023-05-22 00:43:45 +0100

    feat: dynamically change server's IP address

--- frontend/App.js
+++ frontend/App.js
@@ -19,7 +19,7 @@ const Providers = BuildProviderTree([
 export default function App() {
   return (
     <Providers>
-      <SSEClient />
+      {/* <SSEClient /> */}
       <NavigationContainer>
         <NavBar />
       </NavigationContainer>
